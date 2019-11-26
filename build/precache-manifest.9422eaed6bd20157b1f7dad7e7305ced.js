0e80b31faadeb0878adf98a1760cbc18e6d32855
hafidhrendyanto <hafidh.rendyanto@gmail.com>
2019-11-26 19:02:27 +0700

    production

---
+++ build/precache-manifest.9422eaed6bd20157b1f7dad7e7305ced.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "006e4e33c6cfccd7c7a5d2af1ea34ca3",
+    "url": "/bank-pro/index.html"
+  },
+  {
+    "revision": "ca51a83f6a294ff69505",
+    "url": "/bank-pro/static/css/main.fdb3221f.chunk.css"
+  },
+  {
+    "revision": "2f301f7c925cc7816307",
+    "url": "/bank-pro/static/js/2.763827be.chunk.js"
+  },
+  {
+    "revision": "ca51a83f6a294ff69505",
+    "url": "/bank-pro/static/js/main.a56d5f6c.chunk.js"
+  },
+  {
+    "revision": "e1e87cd12fe58cb39bd6",
+    "url": "/bank-pro/static/js/runtime-main.e2f57256.js"
+  },
+  {
+    "revision": "2c13ab9e6ec00612858b6540bf76948d",
+    "url": "/bank-pro/static/media/potrait.2c13ab9e.png"
+  }
+]);
